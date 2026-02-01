babc990b65bf7338f8bf1242f0ca7e15d212e1d6
Harsha <harsharoy07@gmail.com>
2026-02-01 12:48:43 +0530

    created home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className=' w-screen h-screen flex items-center justify-center'>
+      <div className=' w-[300px] h-[90vh] border font-Rubic flex flex-col justify-end p-5'>
+        
+        {/* Routes */}
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+
+      </div>
+    </div>
   )
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+        <h3 className=' font-medium py-2 text-2xl'>Welcome to PopX</h3>
+        <div className=' mb-4'>
+          <p className=' text-sm text-gray-400'>Lorem ipsum dolor sit amet,</p>
+          <p className=' text-sm text-gray-400'>consectetur adipiscing elit,</p>
+        </div>
+        <Link to={'/createAccount'}>
+          <button className=' py-2 text-sm bg-blue-600 text-white rounded-md w-full'>
+              Create Account
+          </button>
+        </Link>
+        <Link to={'/login'}>
+          <button className=' py-2 text-sm bg-purple-200 text-black font-medium rounded-md mt-2 mb-4 w-full'>
+              Already Registered? Login
+          </button>
+        </Link>
+    </div>
+  )
+}
+
+export default Home
